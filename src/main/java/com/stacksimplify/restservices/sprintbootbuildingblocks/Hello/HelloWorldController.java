4412ef937590fefd76f9c41ca4ebb04b7b7cbf83
drew.nicolette <bulking123@gmail.com>
2021-03-29 19:49:37 -0400

    Added bean

---
+++ src/main/java/com/stacksimplify/restservices/sprintbootbuildingblocks/Hello/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.stacksimplify.restservices.sprintbootbuildingblocks.Hello;
+//Controller
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	//Simple Method
+	//URI - /helloworld
+	//GET
+	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping("/helloworld1")
+	public String helloWorld() {
+		return "Hello World1";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Drew","Nicolette","Centreville");
+	}
+
+}
